bc2fc697e85bc1db6d330471d159ad5a51743d16
Mark Ajinwi Nditapah <nditapah.mark@ictuniversity.edu.cm>
2026-04-20 06:14:17 +0100

    farmer dashboard, forcast, trustscores html files + css. modified 1 line on app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def create_app(config_class=Config):
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id))
+        return db.session.get(User, int(user_id))
 
     # ── Register blueprints ──────────────────────────────────
     with app.app_context():
